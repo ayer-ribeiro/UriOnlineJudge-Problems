408e550270ad1a6042a52899e1506bb96b0c1869
Bruno Smarsaro Bazelato <brunosmarsaro@gmail.com>
2015-10-15 23:59:34 -0300

    Adivinha so? Não ta funcionando kkk

--- points/points/main.cpp
+++ points/points/main.cpp
@@ -20,15 +20,40 @@ vector<float> compute_centre(vector<int> p1, vector<int> p2, vector<int> p3){
     float h, k; // h = X and k = Y ---> no reason for this notation!
     float nume, deno;
     // Calculating h
-    nume = ((pow(p1[0],2)) + pow(p1[1],2))*(p2[1]) + ((p1[1])*((pow(p3[0], 2)) + pow(p3[1],2))) + ((pow(p2[0],2)) + pow(p2[1],2));
+    nume = ((pow(p1[0],2)) + pow(p1[1],2))*(p2[1]) + ((p1[1])*((pow(p3[0], 2)) + pow(p3[1],2))) + ((pow(p2[0],2)) + pow(p2[1],2))*(p3[1]) - (p2[1])*((pow(p3[0],2)) + (pow(p3[1],2))) - (p1[1])*((pow(p2[0],2)) + pow(p2[1],2)) - ((pow(p1[0],2)) + (pow(p1[1],2)))*(p3[1]);
+    
+    deno = 2*(p1[0]*p2[1] + p1[1]*p3[0] + p2[0]*p3[1] - p2[1]*p3[0] - p1[1]*p2[0] - p1[0]*p3[1]);
     
     h = nume/deno;
     // Calculating k
+    nume = ((pow(p2[0],2)) + pow(p2[1],2))*p1[0] +((pow(p1[0],2)) + pow(p1[1],2))*p3[0] + ((pow(p3[0], 2)) + pow(p3[1],2))*p2[0] - ((pow(p2[0],2)) + pow(p2[1],2))*p3[0] - ((pow(p1[0],2)) + pow(p1[1],2))*p2[0] - ((pow(p3[0], 2)) + pow(p3[1],2))*p1[0];
     
     k = nume/deno;
     
     vector<float> point = {h,k};
     return point;
+}
+
+bool is_equal(vector<float> p1, vector<float> p2){
+    if (p1[0] == p2[0] && p1[1] == p2[1]) return true;
+    else return false;
+}
+
+int number_points (vector<vector<float>> centre){
+    int max = - 1;
+    int equals;
+    for (int i = 0; i < centre.size(); i++){
+        equals = 0;
+        for (int j = i+ 1; j < centre.size(); j++){
+            if (is_equal(centre[i], centre[j])){
+                equals++;
+            }else break;
+            
+        }
+        if (max < equals) max = equals;
+    }
+    
+    return max;
 }
 
 int main(int argc, const char * argv[]) {
@@ -40,7 +65,7 @@ int main(int argc, const char * argv[]) {
         vector<vector <int>> points;
         points.resize(n_points);
         
-        vector<float> centre;
+        vector<vector<float>> centre;
         
         
         for (int i = 0; i<n_points; i++){
@@ -59,12 +84,12 @@ int main(int argc, const char * argv[]) {
                         continue;
                     } else{
                         vector<float> centre_pos = compute_centre(points[i], points[j], points[k]);
-                        
+                        centre.push_back(centre_pos);
                     }
                 }
             }
         }
-        
+        cout << number_points(centre) << "\n";
     }
     
     cout << n_points << "\n";
