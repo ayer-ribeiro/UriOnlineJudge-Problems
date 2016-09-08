1a9ab9c540b2307bb489b6dd6cd030decdaf0a9d
Ayer Ribeiro de Souza Netto <ayerrsn2@gmail.com>
2016-09-08 01:32:27 -0300

    Create 1008.c

---
+++ 1008.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+     int number;
+     int quant;
+     float valorPorHora;
+     scanf("%d %d %f",&number,&quant,&valorPorHora);
+     printf("NUMBER = %d\nSALARY = U$ %.2f\n",number,quant*valorPorHora);
+
+    return 0;
+}
