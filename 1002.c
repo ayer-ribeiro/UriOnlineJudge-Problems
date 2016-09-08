c5a9aa07170d90755a5927bbb1f188d3ac18d37d
Ayer Ribeiro de Souza Netto <ayerrsn2@gmail.com>
2016-09-08 01:33:08 -0300

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     double pi = 3.14159;
+     double r;
+     scanf("%lf",&r);
+     printf("A=%.4lf\n",pi*r*r);
+
+
+    return 0;
+}
